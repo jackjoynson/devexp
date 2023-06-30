f85aa171da8fcb0eef78af04df09e51340c5bcb6
Jack Joynson <jack@jack.joynson.software>
2023-06-30 20:47:32 +0100

    Prep for react and add stylesheet

--- _config.ts
+++ _config.ts
@@ -1,8 +1,10 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
+import esbuild from "lume/plugins/esbuild.ts";
 
 const site = lume();
 
 site.use(postcss());
+site.use(esbuild({extensions: ['.page.tsx']}));
 
 export default site;
